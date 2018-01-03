1dbdc8fe6d2d03c8dada698c63613420580a43b7
zhalice2011 <zhalice2012@gmail.com>
2018-01-04 01:10:14 +0800

    webpack打包

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,14 +1,36 @@
 //配置webpack
 const path = require('path') //使用相对路径 不要使用绝对路径
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
     entry:{  
-        app:path.join(__dirname,'../client/app') //表示app.js作为项目打包的入口 然后根据app.js里面的依赖关系 弄成关系树 变成一个js文件
+        app:path.join(__dirname,'../client/app.js') //表示app.js作为项目打包的入口 然后根据app.js里面的依赖关系 弄成关系树 变成一个js文件
 
     },
     output:{
         filename:'[name].[hash].js', //输出的文件名
         path:path.join(__dirname,'../dist'), //文件存放路径
-        publicPath:'',//静态资源文件的引用路径  区分是静态资源还是api请求
-
-    }
+        publicPath:''//静态资源文件的引用路径  区分是静态资源还是api请求
+    },
+    // resolve: {
+    //     extensions: ['','.js','.jsx']
+    // },
+    module:{
+        rules:[ //一个数组 数组里面可以配置很多的loader
+            {
+                test: /\.jsx$/, //判断哪一种类型(以jsx结尾的)的文件,都使用下面的loader
+                loader: 'babel-loader' //babel是一个可以编译最新的js语法的工具.编译成es5语法
+                
+            },
+            {
+                test: /\.js$/, //判断哪一种类型(以jsx结尾的)的文件,都使用下面的loader
+                loader: 'babel-loader', //babel是一个可以编译最新的js语法的工具.编译成es5语法
+                exclude:[
+                    path.join(__dirname,'../node_modules')
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin(), // Generates default index.html 
+    ]
 }
---
+++ client/app.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react'
+
+//一个react组件其实就是一个class 而这个calss又继承React的这个class
+export default class App extends React.Component{
+    render() { //return html代码
+        return (
+            <div>This is zhouli de app</div>
+        )
+    }
+}
